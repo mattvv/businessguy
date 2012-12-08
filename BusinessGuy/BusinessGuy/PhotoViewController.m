72ce6e1426d7ca22ec038b821aa2ff16fe97f418
Matt Van Veenendaal <m@mattvv.com>
2012-12-08 11:50:13 -0800

    proper scrolling

--- BusinessGuy/BusinessGuy/PhotoViewController.m
+++ BusinessGuy/BusinessGuy/PhotoViewController.m
@@ -40,7 +40,7 @@
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [self performSegueWithIdentifier:@"viewPhoto" sender:indexPath];
 }
 
